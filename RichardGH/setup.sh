280e221312aa391e827acdd20c5f4790929cb6cd
Richard Agyekum <quadjogh95@gmail.com>
2024-04-04 08:33:10 +0000

    Adding RichardGH's files

---
+++ RichardGH/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda init
+conda activate
+conda create -n funtools
+conda activate funtools
+conda install bwa -c bioconda
+conda install samtools -c  bioconda
+conda install bcftools -c  bioconda
+conda install fastp -c  bioconda
+conda install fastqc -c  bioconda
